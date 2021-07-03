782abcee522354f035efc94c1d239fcec4b35463
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-03 22:01:30 +0530

    initialize the object of the class WifiManager with the method call of the method getApplicationContext() followed by the getSystemService() method

--- app/src/main/java/com/example/wifi_button_app_java/MainActivity.java
+++ app/src/main/java/com/example/wifi_button_app_java/MainActivity.java
@@ -2,13 +2,14 @@ package com.example.wifi_button_app_java;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     Button on, off;
-
+    WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
