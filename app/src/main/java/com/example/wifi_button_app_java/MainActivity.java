6d55e95799a6fbd2147f7f97232c032026220f07
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-03 21:57:52 +0530

    define the objects for the Button class

--- app/src/main/java/com/example/wifi_button_app_java/MainActivity.java
+++ app/src/main/java/com/example/wifi_button_app_java/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.wifi_button_app_java;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button on, off;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
